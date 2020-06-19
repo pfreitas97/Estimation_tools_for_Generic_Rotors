2bcc9bf456366d899a070162b39c97a37925a082
Pfreitas97 <pfreitas@purdue.edu>
2020-06-18 22:54:47 -0400

    initial commit for new version of program

---
+++ rotorPerformance.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 18 11:16:06 2020
+
+@author: pedroaugustofreitasdearaujo
+"""
+
+
+import numpy as np
+
+import scipy.integrate
+
+
+# Class is meant to store geometry of a specific rotor and perform certain tasks
+
+# Maybe functions first would be more illuminating
+
+# class RotorObj:
+    
+#     def __init__(self):
+        
+#         #b,R,C,omega,theta0,cl_cd_alpha) 
+        
+#         self.name = []
+        
+#         self.
+
+
+
+
+# This name will be change when refactoring most likely
+
+# DO'S: final place where all calculations are made, can easily be modified to return
+# Desired information that was not explicitly included.
+
+# Takes in: (b) number of blades, R = Radius (m), C = chord (m), 
+# Omega = Angular V (rad/s), theta0 
+
+#def rotorPerformanceFromGeometry(b,R,C,omega,theta0, thetaMin, cl_cd_alpha):
+
+
+  
+
+
+
+# Step 5 get inflow angle and induced velocity
+
+# Function takes in lift slope curve a, number of blades b,
+# r/R:  a vector of elements displaying the ratio of the blade element to the max radius
+# c/R : a vector of elelements with the ratio of the local chord to the maximum radius
+# theta: a vector of the local twist 
+
+#returns a vector of the induced velocity of every point
+
+
+
+def getInducedVelocity(a,b, cR, rR,theta):
+    
+    # check if cR and theta are either scalars or arrays of the appropriate length
+    
+    assert(np.isscalar(theta) or len(rR) == len(theta))
+    
+    assert(np.isscalar(cR) or len(cR) == len(rR))
+    
+    
+    return  ((a * b * cR) / (16 *np.pi* rR)) * (-1 + np.sqrt(1 + (32 * np.pi * theta * rR) /(a * b * cR)  ) )
+    
+
+# Step 6
+
+
+# theta and induced angle will be in RADIANS
+
+# but the angle of attack returned will be in DEGREES due to historical convention for AOA
+
+def getLocalAOA(theta, inducedAngle):
+    
+    return (theta - np.arctan(inducedAngle)) * (180/np.pi) 
+
+
+
+''' TODO '''
+# Step 7 -- extract cl,cd as a functon of mach number and airfoil Shape 
+
+
+
+# Step 8 
+
+# cl is assumed to be a numpy array of the appropriate length obtained in step 7
+
+def getRunningThrustLoading(b,rR,cR,cl):
+    
+    assert(len(cl) == len(rR))
+    
+    
+    return ((b * (rR**2) * cR * cl)/(2*np.pi))
+
+
+# Step 9
+
+# Integrate removing the assumed hub radius 
+
+def getCTnoTipLoss(dct_dr, rR, x_hub = 0.1):
+    
+    # find index of every element larger than the hub
+    
+    assert(len(dct_dr) == len(rR))
+    
+    indexs = [i for i,l in enumerate(rR) if l > x_hub]
+    
+    rR_toIntegrate = rR[indexs]
+    
+    dct_dr_toIntegrate = dct_dr[indexs] 
+    
+    
+    return scipy.integrate.simps(dct_dr_toIntegrate,rR_toIntegrate)
+
+
+'''TODOO'''
+
+# Step 10. Determine appropriate heuristic for tip loss value
+
+
+
+
+# Step 11
+
+''' TODO REFACTOR TO ACCOUNT FOR POSSSIBLE LACK OF POINTS '''
+
+def getCorrectedCT(CTnoTipLoss, dct_dr, rR, B = 0.9):
+    
+    assert(len(dct_dr) == len(rR))
+    
+    indexs = [i for i,l in enumerate(rR) if l > B]
+    
+    rR_toIntegrate = rR[indexs]
+    
+    dct_dr_toIntegrate = dct_dr[indexs] 
+    
+    
+    return CTnoTipLoss - scipy.integrate.simps(dct_dr_toIntegrate,rR_toIntegrate)
+
+# step 12
+    
+def getRunningProfileTorqueCoefficient(b,rR, cR, cd):
+    
+    assert(len(rR) == len(cd))
+    assert(len(rR) == len(cR))
+    
+    
+    return (b * (rR**3) * cR * cd) / (2*np.pi)
+
+
+# Step 13
+    
+def getProfileTorqueCoefficient(dq_dr,rR):
+    
+    assert(len(dq_dr) == len(rR))
+    
+    return scipy.integrate.simps(dq_dr,rR)
+
+
+
+# Step 14
+
+def getRunningInducedTorqueCoefficient(b,rR,cR,cl,induced_Vel):
+    
+    
+    
+    return (b *  (rR**3) * cR * cl * induced_Vel)/ (2*np.pi)
+
+
+
+# Step 15
+def getInducedTorqueCoefficient(dqi_dr,rR, x_hub = 0.1, B = 0.9):
+    
+    assert(len(dqi_dr) == len(rR))
+    
+    indexs = [i for i,l in enumerate(rR) if l > B and l < x_hub]
+    
+    rR_toIntegrate = rR[indexs]
+    
+    dqi_dr_toIntegrate = dqi_dr[indexs] 
+    
+    return scipy.integrate.simps(dqi_dr_toIntegrate,rR_toIntegrate)
+
+
+
+# Step 16
+
+def computeWake(CT,CQi):
+    
+    
+    return 0
+
+
+# Step 17
+
+def computeDiskLoading(CT,rho,omega,R):
+    
+    return (CT * rho *( omega*R)**2)
+
+
+
+
+
+
+
+a = 2*np.pi
+
+b = 2
+
+cR = 0.2
+
+rR = np.array([0.1,0.3,0.5,0.9])
+
+thits = np.array([0.2,0.2,0.2,0.2])
